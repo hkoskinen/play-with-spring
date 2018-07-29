45e2873beccb2ec2065f374e9d8b444ea2080ba2
Henri Koskinen <hkoskinen@protonmail.com>
2018-07-29 17:38:10 +0300

    Change bean scope to prototype

--- src/main/java/com/gravenium/playwithspring/KnightMain.java
+++ src/main/java/com/gravenium/playwithspring/KnightMain.java
@@ -13,6 +13,17 @@ public class KnightMain {
         System.out.println("My name is " + fielder.getName());
         System.out.println("My email address is " + fielder.getEmail());
 
+        Knight k1 = context.getBean(Knight.class);
+        Knight k2 = context.getBean(Knight.class);
+
+        if (k1 == k2) {
+            System.out.println("Knight references k1 and k2 are pointing to the same object\n" +
+                    "Mem loc for k1: " + k1 + "\nMem loc for k2: " + k2);
+        } else {
+            System.out.println("Knight references k1 and k2 are pointing to a different objects\n" +
+                    "Mem loc for k1: " + k1 + "\nMem loc for k2: " + k2);
+        }
+
         context.close();
     }
 }
